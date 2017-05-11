e9d3479054d61b3f85c03cf52467e8f8e9d34d1d
Osaro Iyoha <saro.iyoha@gmail.com>
2017-05-11 14:24:26 +0100

    car_class update with better logic

--- car_class/car_class.js
+++ car_class/car_class.js
@@ -1,75 +1,51 @@
-
 class Car {
 
-    constructor(_name, _model) {
-        this._name = _name;
-        this._model = _model;
-    }
-    name() {
-        
-        return this._name ;
-    }
-    model(){
-        return this._model
-    }
-    numOfDoors(){
-        if ( this._name === 'Porshe'|| this._name=== 'Koenigsegg')
-        {
-            return 2;
-        }
-        else
-        {
-            return 4;
-        }
-    }
-    drive(bool){
-        if (bool === true)
-        {
-             return true;
-        }
-        else
-        {
-            return false;
-        }
-     
-    }
-    speed(){
-    if (drive(bool)===true){
-      if (this._model==='Truck')
-      {
-        return '77km/h'
-      }
-      else
-      {
-        return '250km/h';
-      }
-  
-    }
-    else{
+    constructor(name,model,type) {
 
-        return '0km/h';
+        this.name = name;
+        this.type = type;
+        this.model = model;
+        this.isSaloon = true;
+        this.numOfWheels = 4;
+        this.speed = '77 km/h';
+        this.numOfDoors = 4;
+
+        if(name === undefined){
+
+            this.name = "General";
+        }
+
+        if(model === undefined){
+
+            this.model = "GM";
+        }
+
+        if(this.type === "trailer"){
+
+            this.numOfWheels = 8;
+            this.isSaloon = false;
+            this.speed = '0 km/h';
+
+        }
+
+        if(this.name === "Porshe" || this.name === "Koenigsegg"){
+            this.numOfDoors = 2;
+        }
     }
-      
+
+    drive(gear){
+
+        if(gear === 5){
+
+            this.speed  = '250 km/h';
+        }
+        else if(gear=== 7){
+
+            this.speed  = '77 km/h';
+        }
+        return new Car()
+
     }
-    isSaloon()
-    {
-       if(this._model.toLowerCase()==='truck'){
-        return false;
-       }
-       else
-       {
-         return true;
-       }
-    }
-    numOfWheels(){
-       if(this._model.toLowerCase()==='truck'){
-        return 8;
-       }
-       else
-       {
-         return 4;
-       }
-    }
+
 }
-
 module.exports = Car;
